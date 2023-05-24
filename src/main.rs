52aacabd90b7f3be0a75d273d41e54c4ff723bca
iljah <ilja200303@gmail.com>
2023-05-24 16:44:45 +0300

    cors update

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 
     let cors = CorsLayer::new()
-        .allow_origin(AllowOrigin::exact("http://localhost:3000".parse().unwrap()))
+        .allow_origin(AllowOrigin::exact("https://cloud-storage-frontend-murex.vercel.app".parse().unwrap()))
         .allow_headers(vec![
         header::ACCEPT,
         header::ACCEPT_LANGUAGE,
